6a69be8f1db8efabb06201fde8b0e418bde442d0
victorbillaud <victorbillaud@gnail.com>
2023-02-14 18:12:22 +0100

    Add types on icon record

--- dashboard/src/components/common/icon/constant.ts
+++ dashboard/src/components/common/icon/constant.ts
@@ -1,9 +1,10 @@
+import React from 'react';
 import ArrowRight from './svg/arrow-right';
 import Bolt from './svg/bolt';
 import Cross from './svg/cross';
 import Spin from './svg/spin';
 
-export const icons = {
+export const icons: Record<string, React.FC<React.SVGProps<SVGSVGElement>>> = {
   'arrow-right': ArrowRight,
   bolt: Bolt,
   spin: Spin,
