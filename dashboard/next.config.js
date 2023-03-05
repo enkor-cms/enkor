306f0e20ed452bf34e400576c1f3485ee6cf8868
victorbillaud <victorbillaud@gmail.com>
2023-03-05 19:29:48 +0100

    Add automatic redirect / to /dashboard

--- dashboard/next.config.js
+++ dashboard/next.config.js
@@ -19,6 +19,15 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/dashboard',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
